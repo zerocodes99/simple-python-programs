a7ff20dc96f443e0d47ea23f97401b0e636bc8a0
zerocodes99 <zerocodes99@gmail.com>
2023-09-01 17:45:32 +0530

    Python program to check if given number is even or odd. (User input)

---
+++ test_even_odd.py
@@ -0,0 +1,10 @@
+def even_odd(x):  # defining function
+    if x % 2 == 0: # condition for even numbers
+        print(f"{x} is a even number.")
+    else:
+        print(f"{x} is a odd number.")
+
+num = int(input("Enter a number to check if it a even or odd: ")) 
+# taking input number from user
+
+even_odd(num) # function calling
